007916c2e54cf5d250224061d163c718bf797e9c
Igor De spirlet <ide-spir@shi-r5-p2.s19.be>
2022-01-18 16:08:56 +0100

    Correction sur atoi et libft.h

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ide-spir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 16:22:57 by ide-spir          #+#    #+#             */
-/*   Updated: 2022/01/12 17:22:32 by ide-spir         ###   ########.fr       */
+/*   Updated: 2022/01/18 15:49:12 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,40 @@ static int	is_space(char c)
 		return (0);
 }
 
+static int	ft_handle_overflow(int res, int neg)
+{
+	if (res == INT_MIN)
+		return (INT_MIN);
+	if (neg == 1)
+		return (-1);
+	else
+		return (0);
+}
+
 int	ft_atoi(const char *str)
 {
-	unsigned long	res;
-	int				neg;
+	long long	res;
+	int			neg;
+	int			i;
 
 	neg = 1;
 	res = 0;
+	i = 0;
 	while (is_space(*str))
-		str++;
-	if (*str == '-')
+		i++;
+	if (str[i] == '-')
 	{
 		neg = -1;
-		str++;
+		i++;
 	}
-	else if (*str == '+')
-		str++;
-	while (*str >= '0' && *str <= '9')
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		res = res * 10 + *str++ - '0';
-		if (res > 2147483647 && neg == 1)
-			return (-1);
-		if (res > 2147483648 && neg == -1)
-			return (0);
+		res = res * 10 + str[i] - '0';
+		i++;
+		if (res < 0)
+			return (ft_handle_overflow(res, neg));
 	}
 	return (neg * res);
 }
